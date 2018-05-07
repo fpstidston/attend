426cb4d3643ba827dd2271180b59da04fe77c159
Fred Stidston <fpstidston@icloud.com>
2018-05-07 07:26:45 +0100

    Home insights

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use \Core\View;
 use \App\Auth;
+use \App\Models\Item;
 
 /**
  * Home controller
@@ -20,6 +21,30 @@ class Home extends \Core\Controller
      */
     public function indexAction()
     {
-        View::renderTemplate('Home/index.html');
+    	$items = (array) Item::getItems('id');
+
+    	if (isset($_SESSION['user_id']) and (count($items)  > 2))	 {
+
+    		$itemsByDate = Item::getItems('id');
+	        $dateStats = Item::getStat($itemsByDate);
+	        $insight[] = Item::getBestDay($dateStats['totals']);
+
+	        $itemsBySubject = Item::getItems('name');
+	        $subjectStats = Item::getStat($itemsBySubject);
+	        $insight[] = Item::getBestSubject($subjectStats['averages']);
+
+	        $insight[] = Item::getRegularestSubject($subjectStats['counts'],$subjectStats['averages']);
+
+	        View::renderTemplate('Home/index.html',[
+        		'insights' => $insight
+        	]);
+
+    	} else {
+
+    		View::renderTemplate('Home/index.html');
+
+    	}
+
+        
     }
 }
